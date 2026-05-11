12bc7d843feaf0f5d1cfcec07a38f290d0daf560
Justin <ju3tin@hotmail.co.uk>
2026-05-11 10:39:28 +0100

    update globalcss

---
+++ app/(game)/layout.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { useMemo } from 'react';
+
+export default function EventsLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const endpoint = 'https://api.devnet.solana.com';
+
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new SolflareWalletAdapter(),
+    ],
+    []
+  );
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          {children}
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+}
