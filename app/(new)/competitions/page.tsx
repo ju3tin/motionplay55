bfc4b7567f6a7d729d89dff399ab85d2040c6639
Justin <ju3tin@hotmail.co.uk>
2026-05-11 18:14:44 +0100

    Fix link syntax for leaderboard navigation

--- app/(new)/competitions/page.tsx
+++ app/(new)/competitions/page.tsx
@@ -331,7 +331,9 @@ export default function CompetitionsPage() {
 
 <div style={{ marginBottom: 16 }}>
   
-<link href=`https://motionplay.vercel.app/leaderboard1?comp=${comp.pubkey}`>
+<link
+  href={`https://motionplay.vercel.app/leaderboard1?comp=${comp.pubkey}`}
+/>
 Leaderboard
 </link>
 
