55c1ddf138665fd31c0c07810853baf0fc011fb5
Justin <ju3tin@hotmail.co.uk>
2026-05-14 12:17:40 +0100

    ,mnmn

---
+++ app/(login)/layout.tsx
@@ -0,0 +1,11 @@
+import { WalletProvider } from '@/providers/WalletProvider';
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <WalletProvider>{children}</WalletProvider>
+      </body>
+    </html>
+  );
+}
