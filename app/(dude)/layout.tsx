5074aaf94ee90c1ecda5b753a9d0bb45ca372fb0
Justin <ju3tin@hotmail.co.uk>
2026-05-17 12:07:21 +0100

    Update layout.tsx

--- app/(dude)/layout.tsx
+++ app/(dude)/layout.tsx
@@ -1,10 +1,16 @@
 import Providers from "@/app/providers";
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <html>
+    <html lang="en">
       <body>
-        <Providers>{children}</Providers>
+        <Providers>
+          {children}
+        </Providers>
       </body>
     </html>
   );
