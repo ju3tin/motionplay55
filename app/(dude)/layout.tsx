824e048839e297ceba9fa940dc343a24ba411a58
Justin <ju3tin@hotmail.co.uk>
2026-05-17 01:02:17 +0100

    Add SolanaProvider component for wallet integration

---
+++ app/(dude)/layout.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import {
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+  BackpackWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+
+export default function SolanaProvider({ children }: any) {
+  const wallets = [
+    new PhantomWalletAdapter(),
+    new SolflareWalletAdapter(),
+    new BackpackWalletAdapter(),
+  ];
+
+  return (
+    <ConnectionProvider endpoint="https://api.devnet.solana.com">
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+}
