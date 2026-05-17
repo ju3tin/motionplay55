4db430967b8999498622193af862be79fdc03bb2
Justin <ju3tin@hotmail.co.uk>
2026-05-17 01:42:22 +0100

    Add Providers component for Solana wallet integration

---
+++ app/providers.tsx
@@ -0,0 +1,15 @@
+// app/providers.tsx
+"use client";
+
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+
+export default function Providers({ children }) {
+  return (
+    <ConnectionProvider endpoint="https://api.devnet.solana.com">
+      <WalletProvider wallets={[]} autoConnect>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+}
