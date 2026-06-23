eca34903cd8732b59775638eac4ad40290772b3f
Justin <ju3tin@hotmail.co.uk>
2026-06-23 09:50:25 +0100

    Create page component for HandsPong game room

---
+++ app/(game)/hands-pong/[roomId]/page.tsx
@@ -0,0 +1,12 @@
+// app/(game)/test-room/[roomId]/page.tsx
+import HandsPong from "./HandsPong";
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ roomId: string }>;
+}) {
+  const { roomId } = await params;
+
+  return <TestRoomClient roomId={roomId} />;
+}
