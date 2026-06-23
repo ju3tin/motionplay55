dc2262c6f6c3b7d432f096bb72790c54cb202219
Justin <ju3tin@hotmail.co.uk>
2026-06-23 09:20:27 +0100

    Update types1.ts

--- lib/pubnub/types1.ts
+++ lib/pubnub/types1.ts
@@ -1,3 +1,36 @@
 export type GameMessage =
-  | { type: "chat"; text: string; userId: string }
-  | { type: "input"; x: number; y: number };
+  | {
+      type: "chat";
+      userId: string;
+      text: string;
+    }
+  | {
+      type: "input";
+      payload: {
+        x: number;
+        y: number;
+      };
+    }
+  | {
+      type: "pong:state";
+      payload: {
+        ball: {
+          x: number;
+          y: number;
+          vx: number;
+          vy: number;
+        };
+        left: number;
+        right: number;
+        scoreL: number;
+        scoreR: number;
+      };
+    }
+  | {
+      type: "join";
+      userId: string;
+    }
+  | {
+      type: "leave";
+      userId: string;
+    };
