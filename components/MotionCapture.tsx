ebe725d4d8302f731cea803bbd77617055b5388d
Justin <ju3tin@hotmail.co.uk>
2026-07-10 22:34:50 +0100

    Update Mediapipe vision tasks URL

--- components/MotionCapture.tsx
+++ components/MotionCapture.tsx
@@ -31,7 +31,7 @@ export default function MotionCapture() {
 
       const vision =
         await FilesetResolver.forVisionTasks(
-          "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.22/wasm"
+          "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision/wasm"
         );
 
 
