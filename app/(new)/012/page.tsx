7304ced5462b4a474dd0e374222d5a67a7ca158a
Justin <ju3tin@hotmail.co.uk>
2026-07-10 23:47:54 +0100

    Create new page for Dance Motion Capture

---
+++ app/(new)/012/page.tsx
@@ -0,0 +1,17 @@
+import MotionCapture3 from "@/components/MotionCapture3";
+
+
+export default function Home(){
+
+  return (
+    <main>
+
+      <h1>
+        Dance Motion Capture
+      </h1>
+
+      <MotionCapture/>
+
+    </main>
+  );
+}
