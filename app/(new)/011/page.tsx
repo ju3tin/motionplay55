b993ba4b33055d37d4c2fbe045381664a685fa1e
Justin <ju3tin@hotmail.co.uk>
2026-07-10 23:20:54 +0100

    Add MotionCapture page for dance motion capture

---
+++ app/(new)/011/page.tsx
@@ -0,0 +1,17 @@
+import MotionCapture from "@/components/MotionCapture2";
+
+
+export default function Home(){
+
+  return (
+    <main>
+
+      <h1>
+        Dance Motion Capture
+      </h1>
+
+      <MotionCapture/>
+
+    </main>
+  );
+}
