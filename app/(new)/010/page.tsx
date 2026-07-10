f92a1a2bd3feba0b9bd6278ed6946f84f186ccb4
Justin <ju3tin@hotmail.co.uk>
2026-07-10 22:02:41 +0100

    Create page.tsx for Dance Motion Capture component

---
+++ app/(new)/010/page.tsx
@@ -0,0 +1,17 @@
+import MotionCapture from "@/components/MotionCapture";
+
+
+export default function Home(){
+
+  return (
+    <main>
+
+      <h1>
+        Dance Motion Capture
+      </h1>
+
+      <MotionCapture/>
+
+    </main>
+  );
+}
