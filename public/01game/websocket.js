46c5c8026f92e7c4993cc3c35c364478c7fa351d
Justin <ju3tin@hotmail.co.uk>
2026-07-10 15:02:07 +0100

    Add WebSocket functionality in websocket.js
    Implement WebSocket connection for game state updates and player management.

---
+++ public/01game/websocket.js
@@ -0,0 +1,73 @@
+const socket = new WebSocket(
+    "wss://YOUR-RENDER-URL.onrender.com"
+);
+
+
+socket.onopen = () => {
+
+    console.log(
+        "WebSocket connected"
+    );
+
+};
+
+
+socket.onmessage = (event)=>{
+
+    const data =
+        JSON.parse(event.data);
+
+
+    if(data.type === "welcome")
+    {
+        console.log(
+            "My player id:",
+            data.id
+        );
+    }
+
+
+    if(data.type === "state")
+    {
+        console.log(
+            "Players:",
+            data.playerCount
+        );
+
+        updatePlayers(data.players);
+    }
+
+};
+
+
+socket.onerror = err => {
+
+    console.log(
+        "Socket error",
+        err
+    );
+
+};
+
+
+
+export function send(data)
+{
+    if(socket.readyState === WebSocket.OPEN)
+    {
+        socket.send(
+            JSON.stringify(data)
+        );
+    }
+}
+
+
+function updatePlayers(players)
+{
+    console.log(
+        players
+    );
+
+    // Later:
+    // update Three.js avatars here
+}
